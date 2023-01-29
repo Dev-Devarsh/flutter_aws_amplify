b71ade0d74ba0be65755148d9c4522638be7c1df
dev-devarsh <180130111055@gecg28.ac.in>
2023-01-29 20:12:38 +0530

    added grapQl api

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "flutteramplifytut": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "flutteramplifytut78fc0af8": {
       "AppClientID": "string",
